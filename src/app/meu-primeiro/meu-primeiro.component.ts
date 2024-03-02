af877401b1464bd6dcd4497dc44ab99c62036752
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-03-01 21:43:28 -0300

    Criando Component 1

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro',
+    standalone: true,
+    templateUrl: './meu-primeiro.component.html'
+})
+export class MeuPrimeiroComponent { }
