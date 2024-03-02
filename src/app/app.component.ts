dc678c1cb2de3ae6deeba924806c919867779fd0
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-03-01 21:43:47 -0300

    Importando componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, MeuPrimeiro2Component, MeuPrimeiroComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Hello World';
-  texting = "Testando os codigos";
+  testando = "Testando os codigos";
 }
