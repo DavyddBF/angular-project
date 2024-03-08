d1d9e76d8244b269f41bf351de533145963e6e92
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-03-07 21:48:58 -0300

    Removendo do projeto

--- src/app/meu-primeiro/meu-primeiro.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'meu-primeiro',
-    standalone: true,
-    templateUrl: './meu-primeiro.component.html'
-})
-export class MeuPrimeiroComponent { }
