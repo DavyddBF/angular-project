50d0a8637cbda792cc1766cecebf8c203acbddc5
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-03-15 16:29:36 -0300

    Importando PropertyBindingComponent ao projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { CursosComponent } from './cursos/cursos.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    CursosComponent
+    CursosComponent,
+    PropertyBindingComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
