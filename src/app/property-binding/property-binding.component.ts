ad6e9afd63b722e0509857200ac1f9885e6be66b
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-03-20 16:00:09 -0300

    Testes

--- src/app/property-binding/property-binding.component.ts
+++ src/app/property-binding/property-binding.component.ts
@@ -25,4 +25,16 @@ export class PropertyBindingComponent {
       return 'Falso'
     }
   }
+
+  getTemplate() {
+    const codigo =  ` 
+      <h1>Site: {{ url }}</h1>
+      <h2>Visite o site <a href="{{ url }}">jw.org</a></h2>
+      <h2>Visite o site <a [href]="url">jw.org</a></h2>
+      <h2>Operações Matemáticas:  {{ valor1 }} + {{ valor2 }} = {{ valor1 + valor2 }}</h2>
+      <h2>A conta  {{ valor1 }} + {{ valor2 }} não dá {{ valor1 + valor2 + getValor() }}</h2>
+      <h2>Isso e isso é {{ getTrueOrFalse() }}</h2>
+    `
+    return document.querySelector('.test')!.innerHTML = codigo;
+  }
 }
