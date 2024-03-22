fcaf8029fd2afd9746f6ccf3fbf8ce0287f22d69
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-03-22 15:47:44 -0300

    Comentando codigo anterior

--- src/app/property-binding/property-binding.component.ts
+++ src/app/property-binding/property-binding.component.ts
@@ -8,33 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './property-binding.component.css'
 })
 export class PropertyBindingComponent {
-  url: string = 'jw.org';
-  valor1: number = 3;
-  valor2: number = 10;
+  // url: string = 'jw.org';
+  // valor1: number = 3;
+  // valor2: number = 10;
 
-  boleano: boolean = true;
+  // boleano: boolean = true;
 
-  getValor(): number {
-    return 1;
-  }
+  // getValor(): number {
+  //   return 1;
+  // }
 
-  getTrueOrFalse(): string {
-    if(this.boleano && true) {
-      return 'Verdadeiro';
-    } else {
-      return 'Falso'
-    }
-  }
-
-  getTemplate() {
-    const codigo =  ` 
-      <h1>Site: {{ url }}</h1>
-      <h2>Visite o site <a href="{{ url }}">jw.org</a></h2>
-      <h2>Visite o site <a [href]="url">jw.org</a></h2>
-      <h2>Operações Matemáticas:  {{ valor1 }} + {{ valor2 }} = {{ valor1 + valor2 }}</h2>
-      <h2>A conta  {{ valor1 }} + {{ valor2 }} não dá {{ valor1 + valor2 + getValor() }}</h2>
-      <h2>Isso e isso é {{ getTrueOrFalse() }}</h2>
-    `
-    return document.querySelector('.test')!.innerHTML = codigo;
-  }
+  // getTrueOrFalse(): string {
+  //   if(this.boleano && true) {
+  //     return 'Verdadeiro';
+  //   } else {
+  //     return 'Falso'
+  //   }
+  // }
 }
