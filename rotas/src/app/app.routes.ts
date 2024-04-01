506b7dda8745612acbdbf8938bad3ae0ef833349
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-04-01 08:45:40 -0300

    Criando rotas

--- rotas/src/app/app.routes.ts
+++ rotas/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'cursos', component: CursosComponent },
+    { path: 'login', component: LoginComponent }
+];
