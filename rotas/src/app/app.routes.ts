a17abb9e54b5958d607c02098850d85040a8c19e
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-04-01 13:06:14 -0300

    Criando uma rota para CursoDetalhe

--- rotas/src/app/app.routes.ts
+++ rotas/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'cursos', component: CursosComponent },
+    { path: 'curso/:id', component: CursoDetalheComponent },
     { path: 'login', component: LoginComponent }
 ];
